2b47b28da5d35d047ff53d15194de3a13ddf3bcf
Bhavani Surepally <bsurepal@Bhavanis-MacBook-Pro.local>
2026-07-08 23:35:35 +0530

    Add GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    formats: ["image/avif", "image/webp"]
-  }
-};
-
-export default nextConfig;/** @type {import('next').NextConfig} */
-const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
